637c59f638af5c1269c734a4ed5a6c7300f60b37
Hafiz <hafizy007@gmail.com>
2020-01-02 10:22:13 +0400

    Add notification

--- price-tracker.py
+++ price-tracker.py
@@ -6,15 +6,16 @@
 # BEFORE USING THE MAIL STUFF
 #   TURN ON LESS SECURE APPS
 #   ENTER TO AND FROM MAIL IN sendMail()
-#   ENTER ACCOUNT PASSWORD
+#   ENTER EMAIL ID AND PASSWORD 
 
 import requests
 from bs4 import BeautifulSoup
 import smtplib
 from datetime import datetime
 import time
+from win10toast import ToastNotifier
 
-URL = 'https://www.amazon.in/AmazonBasics-21-Ltrs-Classic-Backpack/dp/B013TGESIQ?pf_rd_p=bcb4cee7-28fc-4e80-9dd2-4518c354abf0&pd_rd_wg=Myn9T&pf_rd_r=PFASZ0GNMCTPVER5M8D1&ref_=pd_gw_unk&pd_rd_w=CAZUd&pd_rd_r=29f57772-5004-4b0c-bbe1-e678b8391224' # Product link
+URL = 'https://www.amazon.ae/Apple-iPad-10-2-2019-Facetime/dp/B07XL7G4H6/ref=sr_1_2?keywords=ipad&qid=1577943398&sr=8-2' # Product link
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
 
@@ -23,12 +24,13 @@ def checkPrice():
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    title = soup.find(id='productTitle').get_text()
     price = soup.find(id='priceblock_ourprice').get_text()
-    converted_price = float(price[1:5]) # Number part of price
+    converted_price = eval(price[4:9]) # Number part of price
+    converted_price = converted_price[0]*1000 + converted_price[1]
 
-    if converted_price < 850:
-        updateFile(converted_price)
+    if converted_price < 1100:
+        pushNotification(price)
+        #updateFile(price)
         #sendMail()
     #print(converted_price)
     #print(title.strip())
@@ -38,7 +40,11 @@ def updateFile(price):
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     file = open(r'D:\My Stuff\VSCode\Python\misc\pricelist.txt','a')
     file.write('Price: ' + str(price) + ', ' + dt_string)
-    print('updated')
+    file.close()
+
+def pushNotification(price):
+    notify = ToastNotifier()
+    notify.show_toast('Price Drop!', 'Price is now ' + str(price), duration=10)
 
 def sendMail():
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -46,7 +52,7 @@ def sendMail():
     server.starttls()
     server.ehlo()
     
-    server.login('hafizy007@gmail.com', '') # Secord arg is password
+    server.login('', '') # Username and Password
 
     subject = 'Price fell down'
     body = 'Check  ' + URL
@@ -61,6 +67,8 @@ def sendMail():
 
     server.quit()
 
-while True:
+checkPrice()
+
+'''while True:
     checkPrice()
-    time.sleep(60*60*24)
+    time.sleep(60*60*24)'''

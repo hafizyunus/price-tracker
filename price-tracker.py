5e006731b7832b03579a694f023acbab87eb359a
hafizyunus <hafizy007@gmail.com>
2021-05-13 19:25:24 +0400

    added checkRate variable

--- price-tracker.py
+++ price-tracker.py
@@ -1,5 +1,5 @@
 
-# FOR NEW  PRODUCT
+# FOR NEW PRODUCT
 #   UPDATE URL
 #   UPDATE PRICE LENGTH
 
@@ -15,7 +15,9 @@ from datetime import datetime
 import time
 from win10toast import ToastNotifier
 
-URL = 'https://www.amazon.ae/Apple-iPad-10-2-2019-Facetime/dp/B07XL7G4H6/ref=sr_1_2?keywords=ipad&qid=1577943398&sr=8-2' # Product link
+checkRate = 3
+
+URL = '' # Product link
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
 
@@ -72,4 +74,4 @@ checkPrice()
 
 '''while True:
     checkPrice()
-    time.sleep(60*60*24)'''
+    time.sleep(60*60*24/checkRate)'''
